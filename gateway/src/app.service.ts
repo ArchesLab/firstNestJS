3b95d49fcfe46b780658d9cd32a2e1dd38b8515a
Camillalalala <camilla4clubs@gmail.com>
2026-01-06 22:49:43 -0800

    Gateway: Installed nestjs/config, updated modules, and replaced hardcoded urls with configservice

--- gateway/src/app.service.ts
+++ gateway/src/app.service.ts
@@ -1,17 +1,33 @@
 // gateway/src/app.service.ts
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
 import { User, Club } from './types';
 
 @Injectable()
 export class AppService {
+  private readonly usersServiceBase: string;
+  private readonly clubsServiceBase: string;
+
+  constructor(private readonly configService: ConfigService) {
+    const usersURL = this.configService.get<string>('USERS_SERVICE_URL');
+    const clubsURL = this.configService.get<string>('CLUBS_SERVICE_URL');
+    if (!usersURL || !clubsURL) {
+      throw new Error(
+        'Critical Environment Variable USERS_SERVICE_URL or CLUBS_SERVICE_URL is missing!',
+      );
+    }
+    this.usersServiceBase = usersURL;
+    this.clubsServiceBase = clubsURL;
+  }
+
   async getUsers(): Promise<User[]> {
-    const res = await axios.get<User[]>('http://localhost:3005/users');
+    const res = await axios.get<User[]>(`${this.usersServiceBase}/users`);
     return res.data;
   }
 
   async getClubs(): Promise<Club[]> {
-    const res = await axios.get<Club[]>('http://localhost:3001/clubs');
+    const res = await axios.get<Club[]>(`${this.clubsServiceBase}/clubs`);
     return res.data;
   }
 }
