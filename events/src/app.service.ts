65276e32ad6271e5fbc74e68c85cc8660470d4ab
Camillalalala <camilla4clubs@gmail.com>
2026-01-06 22:14:31 -0800

    Events: Installed nestjs/config, updated modules, and replaced hardcoded urls with configservice

--- events/src/app.service.ts
+++ events/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import {
   EMAIL_TEMPLATES,
   type EmailTemplate,
@@ -7,12 +8,26 @@ import axios from 'axios';
 
 @Injectable()
 export class AppService {
+  private readonly notificationsBaseUrl: string;
+  private readonly clubsBaseUrl: string;
+
+  constructor(private readonly configService: ConfigService) {
+    const notificationsURL = this.configService.get<string>(
+      'NOTIFICATIONS_SERVICE_URL',
+    );
+    const clubsURL = this.configService.get<string>('CLUBS_SERVICE_URL');
+    if (!notificationsURL || !clubsURL) {
+      throw new Error(
+        'Critical Environment Variable NOTIFICATIONS_SERVICE_URL is missing!',
+      );
+    }
+    this.notificationsBaseUrl = notificationsURL;
+    this.clubsBaseUrl = clubsURL;
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
-
-  private readonly notificationsBaseUrl = 'http://localhost:3004';
-  private readonly clubsBaseUrl = 'http://localhost:3001';
 
   getEmailTemplate(key: string): EmailTemplate {
     const tpl = EMAIL_TEMPLATES[key];
